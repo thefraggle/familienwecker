7a734ed10508cb67d249cfde712e27a7a0cbb487
Daniel Notthoff <daniel.notthoff@gmail.com>
2026-03-12 14:09:37 +0100

    chore: bump version to 0.8.3-dev

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     alias(libs.plugins.google.services)
 }
 
-val appVersion = "0.8.2"
+val appVersion = "0.8.3-dev"
 
 val commitHash = providers.gradleProperty("commitHash").getOrElse("dev")
 val commitDate = providers.gradleProperty("commitDate").getOrElse("dev")
