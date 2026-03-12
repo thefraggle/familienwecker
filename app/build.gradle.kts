c1a43da967b5aef92605115d761f6977ec0d82cb
Daniel Notthoff <daniel.notthoff@gmail.com>
2026-03-12 16:18:08 +0100

    chore: nächste Entwicklungsversion 0.9.2-dev

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     alias(libs.plugins.google.services)
 }
 
-val appVersion = "0.9.1"
+val appVersion = "0.9.2-dev"
 
 val commitHash = providers.gradleProperty("commitHash").getOrElse("dev")
 val commitDate = providers.gradleProperty("commitDate").getOrElse("dev")
