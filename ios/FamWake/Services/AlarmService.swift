2533a38470673127909298c41b59273b60c7797d
Daniel Notthoff <daniel.notthoff@me.com>
2026-06-02 20:33:42 +0200

    fix(ios): restore OpenFamWakeIntent to open app from AlarmKit UI

--- ios/FamWake/Services/AlarmService.swift
+++ ios/FamWake/Services/AlarmService.swift
@@ -4,6 +4,33 @@ import AVFoundation
 import AlarmKit
 import ActivityKit
 import SwiftUI
+import AppIntents
+
+struct OpenFamWakeIntent: LiveActivityIntent {
+    static var title: LocalizedStringResource = "Wecker beenden"
+    static var openAppWhenRun: Bool = true
+    
+    @Parameter(title: "Member Name")
+    var memberName: String
+    
+    init() {}
+    
+    init(memberName: String) {
+        self.memberName = memberName
+    }
+    
+    func perform() async throws -> some IntentResult {
+        let name = memberName
+        DispatchQueue.main.async {
+            NotificationCenter.default.post(
+                name: .showRingingView,
+                object: nil,
+                userInfo: ["memberId": "unknown", "memberName": name]
+            )
+        }
+        return .result()
+    }
+}
 
 struct FamWakeAlarmMetadata: AlarmMetadata {
     var memberId: String
@@ -87,6 +114,8 @@ final class AlarmService: ObservableObject {
                 let config = AlarmManager.AlarmConfiguration.alarm(
                     schedule: Alarm.Schedule.fixed(wakeUpTime),
                     attributes: attributes,
+                    stopIntent: OpenFamWakeIntent(memberName: memberName),
+                    secondaryIntent: nil,
                     sound: finalSoundNameToUse == nil ? .default : .named(finalSoundNameToUse!)
                 )
                 
