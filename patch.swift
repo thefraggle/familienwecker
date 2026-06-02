e55f64711638dba6d3baea1e7f23b27739dd12bc
Daniel Notthoff <daniel.notthoff@me.com>
2026-06-02 20:51:21 +0200

    fix(ios): properly configure AlarmKit secondaryButtonBehavior to fix Code=0 crash

--- patch.swift
+++ patch.swift
@@ -1,59 +1,15 @@
-import Foundation
-import Combine
-import AVFoundation
-import AlarmKit
-import ActivityKit
-import SwiftUI
-import AppIntents
-
-struct OpenFamWakeIntent: LiveActivityIntent {
-    static var title: LocalizedStringResource = "Wecker beenden"
-    static var openAppWhenRun: Bool = true
-    
-    @Parameter(title: "Member Name")
-    var memberName: String
-    
-    init() {}
-    
-    init(memberName: String) {
-        self.memberName = memberName
-    }
-    
-    func perform() async throws -> some IntentResult {
-        let name = memberName
-        DispatchQueue.main.async {
-            NotificationCenter.default.post(
-                name: .showRingingView,
-                object: nil,
-                userInfo: ["memberId": "unknown", "memberName": name]
-            )
-        }
-        return .result()
-    }
-}
-
-struct SnoozeFamWakeIntent: LiveActivityIntent {
-    static var title: LocalizedStringResource = "Snooze"
-    static var openAppWhenRun: Bool = false
-    
-    @Parameter(title: "Member Name")
-    var memberName: String
-    
-    init() {}
-    
-    init(memberName: String) {
-        self.memberName = memberName
-    }
-    
-    func perform() async throws -> some IntentResult {
-        let name = memberName
-        DispatchQueue.main.async {
-            NotificationCenter.default.post(
-                name: NSNotification.Name("snoozeAlarmFromNotification"),
-                object: nil,
-                userInfo: ["memberId": "unknown", "memberName": name, "snoozeTime": Date().addingTimeInterval(300)]
-            )
-        }
-        return .result()
-    }
-}
+                let alert: AlarmPresentation.Alert
+                if #available(iOS 26.1, *) {
+                    alert = AlarmPresentation.Alert(
+                        title: LocalizedStringResource(stringLiteral: memberName),
+                        secondaryButton: AlarmButton(text: "Snooze", textColor: .white, systemImageName: "zzz"),
+                        secondaryButtonBehavior: .custom
+                    )
+                } else {
+                    alert = AlarmPresentation.Alert(
+                        title: LocalizedStringResource(stringLiteral: memberName),
+                        stopButton: AlarmButton(text: "Dismiss", textColor: .white, systemImageName: "stop.circle"),
+                        secondaryButton: AlarmButton(text: "Snooze", textColor: .white, systemImageName: "zzz"),
+                        secondaryButtonBehavior: .custom
+                    )
+                }
